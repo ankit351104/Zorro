be5045c8838cfc184fe02b49842f0e2501b30a38
ankit351104 <91881708+ankit351104@users.noreply.github.com>
2023-05-08 20:59:33 +0530

    Update Constants.kt

--- app/src/main/java/com/example/zorro/Constants/Constants.kt
+++ app/src/main/java/com/example/zorro/Constants/Constants.kt
@@ -3,7 +3,7 @@ package com.example.zorro.Constants
 class Constants {
     companion object{
         const val BASE_URL = "https://fcm.googleapis.com"
-        const val SERVER_KEY = "AAAAGDoPiSA:APA91bEMjfB4dQr539_DzoEkSnl0-AEyMJCdRgMEx4Shx99o7nvXlfleYASpkzDhQ4hGWl6A4raLT2hFVRr8bgteTyDHKBKASmWhKkJXRsj6FaK2D-jRGkY5Ih2JY5duyEW7v5eKr3bZ" // get firebase server key from firebase project setting
+        const val SERVER_KEY = "HIDING SERVER KEY FOR SECURITY PURPOSES" // get firebase server key from firebase project setting
         const val CONTENT_TYPE = "application/json"
     }
 }
